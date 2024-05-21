1860217de1c05d8e2e1732be84681f4e0457ca8e
Paul Mourer <paul.mourer@onxmaps.com>
2024-05-20 18:48:29 -0600

    CSR Hack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Remove at some point...
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
