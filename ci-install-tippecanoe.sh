d61b1c5bfb4ee6383d5240005901d6e219455908
Paul Mourer <paul.mourer@onxmaps.com>
2024-05-21 20:51:18 -0600

    We're inside the tippecanoe repo still

--- ci-install-tippecanoe.sh
+++ ci-install-tippecanoe.sh
@@ -4,4 +4,4 @@ git clone --depth 1 --branch "$TIPPECANOE_VERSION" https://github.com/felt/tippe
 cd tippecanoe-repo
 make -j
 echo "Copying to tile server directory..."
-cp tippecanoe ./tile-server/tippecanoe
+cp tippecanoe ../tile-server/tippecanoe
