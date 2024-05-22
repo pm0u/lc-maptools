a54cf8fa2d15c6a65f0ac907fdcc216421bc9b38
Paul Mourer <paul.mourer@onxmaps.com>
2024-05-21 20:49:35 -0600

    Fix file copy

--- ci-install-tippecanoe.sh
+++ ci-install-tippecanoe.sh
@@ -3,4 +3,5 @@
 git clone --depth 1 --branch "$TIPPECANOE_VERSION" https://github.com/felt/tippecanoe.git tippecanoe-repo
 cd tippecanoe-repo
 make -j
-cp tippecanoe ./tile-server
+echo "Copying to tile server directory..."
+cp tippecanoe ./tile-server/tippecanoe
