9071f177e840d1d0573688992c90fd68d919e7ae
Paul Mourer <paul.mourer@onxmaps.com>
2024-05-27 07:25:27 -0600

    Check in again the case sensitive ones

---
+++ app/src/app/query/[lngLat]/query-card/feature-info.tsx
@@ -0,0 +1,34 @@
+import { MapboxGeoJSONFeature } from "mapbox-gl";
+
+export const FeatureInfo = ({
+  feature,
+  className,
+}: {
+  feature: MapboxGeoJSONFeature;
+  className?: string;
+}) => {
+  // @ts-expect-error
+  const { NAME, ...propertiesToDisplay } = feature.properties;
+  return (
+    <div className={className}>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(propertiesToDisplay).map(([p, v]) => (
+              <tr key={p} className="hover">
+                <td>{p}</td>
+                <td>{v}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
