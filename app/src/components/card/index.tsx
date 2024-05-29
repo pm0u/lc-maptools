977fffcb9eaecc57aebbf3bb1218974ad603d852
Paul Mourer <paul@bypaul.dev>
2024-05-29 10:47:28 -0600

    clean up tile data, unlimit tile size, query card layout for more fields

--- app/src/components/card/index.tsx
+++ app/src/components/card/index.tsx
@@ -3,7 +3,7 @@ import { cva } from "class-variance-authority";
 
 const card = cva(
   [
-    "card absolute top-0 pb-8 pt-4 px-4 w-1/4 m-0 drop-shadow-hover gap-4 pointer-events-auto",
+    "card absolute top-0 pb-8 pt-4 px-4 w-1/4 m-0 drop-shadow-hover gap-4 pointer-events-auto max-h-full flex flex-col bg-base-100",
   ],
   {
     variants: {
