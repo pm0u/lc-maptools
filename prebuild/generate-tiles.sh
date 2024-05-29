5a5b8b9c736a244926c99ae05e6b9706539a32c8
Paul Mourer <paul@bypaul.dev>
2024-05-29 01:35:24 -0600

    GH actions (#1)
    * GH actions
    
    * proper setup
    
    * Correct cache key & yarn install command
    
    * repo
    
    * try ssh
    
    * reorder
    
    * Use checkout action
    
    * set the shell
    
    * sanity check
    
    * combine the cd
    
    * Skip the global install
    
    * cache tippecanoe
    
    * add to pr too
    
    * use step context
    
    * proper action path
    
    * local tippecanoe -v
    
    * public doesnt exist since it has no content
    
    * don't use firebase cli
    
    * Mapbox secret token
    
    * Cache id working
    
    * proper cache id
    
    * try cache hit again
    
    * cache again
    
    * Try with shell script
    
    * fix pr action
    
    * Use repo secret for tippecanoe version
    
    * Cache tile data too
    
    * Add to pr too
    
    * spacing
    
    * use proper cache key?
    
    * add to pr too
    
    * use secur env format
    
    * tweaks, proper order
    
    * actually proper order
    
    * remove unused action, test tile caching
    
    * Include tiles in cache-key
    
    * Add branch to cache key
    
    * use yarn
    
    * tippecanoe from env
    
    * Use ENV in shell script
    
    * log tile info
    
    * Actually install
    
    * proper run
    
    * add shell
    
    * Cache proper?
    
    * Tree the tiles
    
    * Fetch the actual tiles from current deploy
    
    * Skip tree
    
    * Split up actions for reusability
    
    * Fix node action reference
    
    * Update prod deploy actions

--- prebuild/generate-tiles.sh
+++ prebuild/generate-tiles.sh
@@ -2,5 +2,11 @@
 # Run from project root!
 
 rm -rf public/tiles
-mkdir public/tiles
-tippecanoe -ps -pT -z18 -Z10 -pC -e public/tiles generated/tax_parcels.json generated/public_land.json --force
+mkdir -p public/tiles
+
+if [ -f "./tippecanoe" ]; then
+  echo "Using local tippecanoe executable"
+  ./tippecanoe -ps -pT -z18 -pC -e public/tiles generated/tax_parcels.json generated/public_land.json --force
+else
+  tippecanoe -ps -pT -z18 -pC -e public/tiles generated/tax_parcels.json generated/public_land.json --force
+fi
