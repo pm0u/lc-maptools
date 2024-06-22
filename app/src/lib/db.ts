a0683486c6c02e2815e784c042c5daa29d88158b
Paul Mourer <paul@bypaul.dev>
2024-06-21 19:18:28 -0600

    Connect via string or object

--- app/src/lib/db.ts
+++ app/src/lib/db.ts
@@ -1,14 +1,16 @@
 import postgres from "postgres";
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, DATABASE_URL } = process.env;
 
-export const sql = postgres({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-});
+export const sql = DATABASE_URL
+  ? postgres(DATABASE_URL)
+  : postgres({
+      host: PGHOST,
+      database: PGDATABASE,
+      username: PGUSER,
+      password: PGPASSWORD,
+      port: 5432,
+    });
 
 export const selectAll = () => {
   return sql`SELECT * FROM eastside_reroutes`;
