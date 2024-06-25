d4551df5eda5968086b6e546a57cba87505bc367
Paul Mourer <paul@bypaul.dev>
2024-06-24 21:55:48 -0600

    Better tile query, param assertions

--- src/app/tiles/[z]/[x]/[y]/route.ts
+++ src/app/tiles/[z]/[x]/[y]/route.ts
@@ -1,77 +1,52 @@
-import { sql } from "@/lib/db";
+import { tileQuery } from "@/lib/tile";
+
+type TileParams = { z: string; x: string; y: string };
+
+const isNumberString = (val: any) => {
+  // Not a number
+  if (isNaN(Number(val))) {
+    return false;
+  }
+  // A decimal number
+  if (parseFloat(val) !== parseInt(val)) {
+    return false;
+  }
+  // maybe a weird hex number
+  if (/[^0-9]/.test(val)) {
+    return false;
+  }
+  return true;
+};
+
+function assertTileParams(params: object): asserts params is TileParams {
+  if (!("x" in params) || !("y" in params) || !("z" in params))
+    throw Error("Invalid params");
+  if (
+    !isNumberString(params.x) ||
+    !isNumberString(params.y) ||
+    !isNumberString(params.z)
+  )
+    throw Error("Invalid params");
+}
 
 export async function GET(
   _request: Request,
   { params }: { params: { z: string; x: string; y: string } }
 ) {
-  const { z, x, y } = params;
+  try {
+    assertTileParams(params);
+    const { z, x, y } = params;
 
-  /**
-   * Took a lot to make this efficient with multiple layers & reducing loops
-   * A combination of multiple sources but this was the most helpful
-   * https://stackoverflow.com/a/66287690
-   *
-   * @todo create a helper to construct the subquery blocks
-   */
-  const tile = await sql`
-    with data as
+    const tile = await tileQuery({ z, x, y });
 
-    (
-      select ST_AsMVTGeom(geom, ST_TileEnvelope(${z}, ${x}, ${y})) as geom,
-      ogc_fid as id,
-      'tax_parcels' as layer,
-      jsonb_build_object(
-        'acres', acres,   
-        'NAME', name
-      ) as features
-      from tax_parcels
-      WHERE geom && ST_TileEnvelope(${z}, ${x}, ${y}, margin => (64.0 / 4096))
-
-      union
-
-      select ST_AsMVTGeom(geom, ST_TileEnvelope(${z}, ${x}, ${y})) as geom,
-      ogc_fid as id,
-      'public_land' as layer,
-      jsonb_build_object(
-        'adm_manage', adm_manage
-      ) as features
-      from public_land
-      WHERE geom && ST_TileEnvelope(${z}, ${x}, ${y}, margin => (64.0 / 4096))
-
-      union
-
-      select ST_AsMVTGeom(geom, ST_TileEnvelope(${z}, ${x}, ${y})) as geom,
-      ogc_fid as id,
-      'eastside_reroutes' as layer,
-      jsonb_build_object(
-        'name', name,
-        'description', description
-      ) as features
-
-      from eastside_reroutes
-      WHERE geom && ST_TileEnvelope(${z}, ${x}, ${y}, margin => (64.0 / 4096))
-    ),
-    layers as 
-    (
-      select
-        ST_AsMVT(
-          data.*,
-          layer,
-          4096,
-          'geom',
-          'id'
-        ) AS mvt
-      FROM
-        data
-      GROUP BY
-        layer
-      )
-      select string_agg(mvt, '') as geom from layers;
-    `;
-
-  const res = new Response(tile[0].geom, {
-    status: 200,
-    headers: { "Content-Type": "application/x-protobuf" },
-  });
-  return res;
+    const res = new Response(tile[0].mvt, {
+      status: 200,
+      headers: { "Content-Type": "application/x-protobuf" },
+    });
+    return res;
+  } catch (e) {
+    console.error(params, e);
+    const res = new Response(null, { status: 404 });
+    return res;
+  }
 }
---
+++ src/lib/tile.ts
@@ -0,0 +1,52 @@
+import { sql } from "@/lib/db";
+
+export const layerDefs = {
+  tax_parcels: ["NAME", "acres"],
+  public_land: ["adm_manage"],
+};
+
+export const allLayers = Object.keys(layerDefs) as Array<
+  keyof typeof layerDefs
+>;
+
+export const queryLayer = ({
+  z,
+  x,
+  y,
+  layer,
+  fields,
+}: {
+  z: string;
+  x: string;
+  y: string;
+  layer: string;
+  fields: string[];
+}) => /* sql */ `
+  (SELECT ST_AsMVT(q, '${layer}', 4096, 'geom') AS l FROM
+        (SELECT ST_AsMvtGeom(
+            geom,
+            ST_TileEnvelope(${z}, ${x}, ${y}),
+            4096,
+            64,
+            true
+            ) AS geom, ${fields.join(", ")}
+       FROM ${layer} WHERE (geom && ST_TileEnvelope(${z}, ${x}, ${y}))) AS q)
+  `;
+
+export const tileQuery = ({
+  z,
+  x,
+  y,
+  layers = allLayers,
+}: {
+  z: string;
+  x: string;
+  y: string;
+  layers?: Array<keyof typeof layerDefs>;
+}) =>
+  sql`select (${sql.unsafe(
+    layers
+      .map((layer) => queryLayer({ z, x, y, layer, fields: layerDefs[layer] }))
+      .join(" || ")
+      .replace(/\s+/g, " ")
+  )}) as mvt`;
