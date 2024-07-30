10973d0686585924999f957353b91b7d9827b224
Paul Mourer <paul@bypaul.dev>
2024-07-30 06:42:00 -0600

    data processing

--- data/constants.ts
+++ data/constants.ts
@@ -3,3 +3,6 @@ export const LCMD_QUERY_ENDPOINT =
 
 export const LCPL_QUERY_ENDPOINT =
   "https://services1.arcgis.com/38PTfoP8IjlBsxZN/arcgis/rest/services/Federal_Lands_ALL_Lake/FeatureServer/0/query";
+
+export const LCZ_QUERY_ENDPOINT =
+  "https://services1.arcgis.com/38PTfoP8IjlBsxZN/arcgis/rest/services/County_Zoning_2022/FeatureServer/0/query";
