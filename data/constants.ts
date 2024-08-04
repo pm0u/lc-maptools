d9955b1220b113194fba545cd28f908b6e7a232c
Paul Mourer <paul@bypaul.dev>
2024-08-04 15:12:23 -0600

    County zoning imports

--- data/constants.ts
+++ data/constants.ts
@@ -5,4 +5,4 @@ export const LCPL_QUERY_ENDPOINT =
   "https://services1.arcgis.com/38PTfoP8IjlBsxZN/arcgis/rest/services/Federal_Lands_ALL_Lake/FeatureServer/0/query";
 
 export const LCZ_QUERY_ENDPOINT =
-  "https://services1.arcgis.com/38PTfoP8IjlBsxZN/arcgis/rest/services/County_Zoning_2022/FeatureServer/0/query";
+  "https://services1.arcgis.com/38PTfoP8IjlBsxZN/arcgis/rest/services/CountyZoning2024/FeatureServer/0/query";
